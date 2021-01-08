40ff0704da87210967276c87de6b8eae61c4cece
lemaitre <Thomas.lemaitre.78420@gmail.com>
2021-01-08 18:07:40 +0100

    mise a jour de bases

--- The-Lab/main.py
+++ The-Lab/main.py
@@ -2,12 +2,13 @@
 
 #import
 from graphics import *
-from moteur import *
+from m import *
+from moteur.map import *
 
 def main():
     init_graphic(500,500)
     run = 1
-    map = Map(50,50)
+    map = Map(500,500)
     while run == 1:
         draw_circle(Point(250,250),39,white)
         run = eventMenu()
--- The-Lab/moteur/map.py
+++ The-Lab/moteur/map.py
@@ -7,10 +7,14 @@ class Map(pygame.sprite.Group):
         super().__init__()
         self.x = tx
         self.y = ty
+        self.tileslist = []  #liste d'objet des tiles
+        self.nbtile = 0
+        self.encodageMap = []
         self.coordMap =[]
-        for x in range(tx/50):
-            for y in range(ty/50):
+        for x in range(tx//50):
+            for y in range(ty//50):
                 self.coordMap.append((0+50*x,0+50*y))
+                print((0+50*x,0+50*y))
 
     def load(self,filename):
         fichier = Fichier(filename)
@@ -22,6 +26,24 @@ class Map(pygame.sprite.Group):
             self.encodageMap[i] = self.encodageMap[i].split(",")
         self.__mapBuild()
         self.__listrectBuild()
+
+    def __mapBuild(self):
+        for i in range(len(self.encodageMap)):
+            for k in range(len(self.encodageMap[0])):
+                if self.encodageMap[i][k] != 'None':
+                    x,y = 0+50*k,0+50*i
+                    if self.encodageMap[i][k] == 'c':
+                        self.listCoin.add(Coin(x,y))
+                    else:
+                        tile = Tile(int(self.encodageMap[i][k]),x,y)
+                        self.add(tile)
+                        self.tileslist.append(tile)
+        self.nbtile = len(self.tileslist)
+
+    def __listrectBuild(self):
+        for i in range(self.nbtile):
+            rect = self.get_rect(i)
+            self.listRect.append([(rect.x,rect.y),(rect.x+rect.width,rect.y),(rect.x,rect.y+rect.height),(rect.x+rect.width,rect.y+rect.height)])
 
 
 
